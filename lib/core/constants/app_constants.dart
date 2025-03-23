7d8becba469101a0e50eaddeb2e090129cdfcbc3
mouraa0 <jpcmouraa@gmail.com>
2025-03-23 19:11:49 -0300

    feat: app constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  static const double defaultPadding = 16.0;
+  static const double defaultMargin = 16.0;
+  static const double headingFontSize = 24.0;
+  static const double headingSmallerFontSize = 20.0;
+  static const double bodyFontSize = 14.0;
+  static const double frameHeight = 80.0;
+
+  static const double borderRadius = 14.0;
+}
