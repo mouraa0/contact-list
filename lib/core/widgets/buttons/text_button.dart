496eb8cdbbe4d9886cae61a9a905129666aee933
mouraa0 <jpcmouraa@gmail.com>
2025-03-23 19:39:44 -0300

    feat: text button

---
+++ lib/core/widgets/buttons/text_button.dart
@@ -0,0 +1,29 @@
+import 'package:contact_list/core/constants/app_constants.dart';
+import 'package:flutter/material.dart';
+
+class AppTextButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const AppTextButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      borderRadius: BorderRadius.circular(AppConstants.borderRadius),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: Text(
+          text.toUpperCase(),
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+            decoration: TextDecoration.underline,
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+      ),
+    );
+  }
+}
