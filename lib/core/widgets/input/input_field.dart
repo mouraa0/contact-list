729fe43b685184613bc2745780f0b0d414d6241a
mouraa0 <jpcmouraa@gmail.com>
2025-03-23 19:39:54 -0300

    feat: add input header to input field

--- lib/core/widgets/input/input_field.dart
+++ lib/core/widgets/input/input_field.dart
@@ -1,7 +1,9 @@
+import 'package:contact_list/core/constants/app_constants.dart';
 import 'package:flutter/material.dart';
 
 class InputField extends StatelessWidget {
   final String hintText;
+  final String headerText;
   final String? Function(String?)? validator;
   final TextEditingController? controller;
   final bool isPassword;
@@ -12,27 +14,43 @@ class InputField extends StatelessWidget {
     this.validator,
     this.controller,
     this.isPassword = false,
+    required this.headerText,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      obscureText: isPassword,
-      decoration: InputDecoration(
-        hintText: hintText,
-        filled: true,
-        fillColor: Theme.of(context).inputDecorationTheme.fillColor,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.0),
-          borderSide: const BorderSide(
-            color: Colors.transparent,
+    return Column(
+      spacing: 16,
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            headerText,
+            style: TextStyle(
+              fontSize: AppConstants.headingSmallerFontSize,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-        contentPadding:
-            const EdgeInsets.symmetric(vertical: 14.0, horizontal: 12.0),
-      ),
-      validator: validator,
+        TextFormField(
+          controller: controller,
+          obscureText: isPassword,
+          decoration: InputDecoration(
+            hintText: hintText,
+            filled: true,
+            fillColor: Theme.of(context).inputDecorationTheme.fillColor,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              borderSide: const BorderSide(color: Colors.transparent),
+            ),
+            contentPadding: const EdgeInsets.symmetric(
+              vertical: 14.0,
+              horizontal: 12.0,
+            ),
+          ),
+          validator: validator,
+        ),
+      ],
     );
   }
 }
