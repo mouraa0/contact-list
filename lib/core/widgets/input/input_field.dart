3601acbbc41b8e75336eb097eacf2f0cd1f35dfb
mouraa0 <jpcmouraa@gmail.com>
2025-03-25 19:51:26 -0300

    feat: edits to input field

--- lib/core/widgets/input/input_field.dart
+++ lib/core/widgets/input/input_field.dart
@@ -1,43 +1,46 @@
-import 'package:contact_list/core/constants/app_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class InputField extends StatelessWidget {
   final String hintText;
+  final IconData? prefix;
   final String headerText;
+  final FocusNode? focusNode;
   final String? Function(String?)? validator;
   final TextEditingController? controller;
   final bool isPassword;
+  final List<TextInputFormatter> inputFormatters;
+  final void Function(String?)? onChanged;
 
   const InputField({
     super.key,
     this.hintText = '',
+    this.headerText = '',
     this.validator,
     this.controller,
+    this.focusNode,
+    this.prefix,
     this.isPassword = false,
-    required this.headerText,
+    this.onChanged,
+    this.inputFormatters = const [],
   });
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      spacing: 16,
+      spacing: 5,
       children: [
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            headerText,
-            style: TextStyle(
-              fontSize: AppConstants.headingSmallerFontSize,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
+        if (headerText.isNotEmpty)
+          Align(alignment: Alignment.centerLeft, child: Text(headerText)),
         TextFormField(
           controller: controller,
           obscureText: isPassword,
+          inputFormatters: inputFormatters,
+          focusNode: focusNode,
           decoration: InputDecoration(
             hintText: hintText,
             filled: true,
+            prefixIcon: prefix != null ? Icon(prefix) : null,
             fillColor: Theme.of(context).inputDecorationTheme.fillColor,
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(8.0),
@@ -48,6 +51,7 @@ class InputField extends StatelessWidget {
               horizontal: 12.0,
             ),
           ),
+          onChanged: onChanged,
           validator: validator,
         ),
       ],
