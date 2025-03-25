45bcf8e4b24c739ec0e616fecda7d643af339671
mouraa0 <jpcmouraa@gmail.com>
2025-03-25 20:07:47 -0300

    feat: contact datasource

---
+++ lib/modules/contacts/data/datasource/contact_datasource.dart
@@ -0,0 +1,84 @@
+import 'dart:convert';
+
+import 'package:contact_list/core/error/failure.dart';
+import 'package:contact_list/modules/auth/domain/entities/auth_entity.dart';
+import 'package:contact_list/modules/contacts/data/models/contact_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class ContactDatasource {
+  Future<List<ContactModel>?> getContacts(AuthEntity user);
+  Future<void> addContact(ContactModel contact, AuthEntity user);
+  Future<void> deleteContact(ContactModel contact, AuthEntity user);
+  Future<void> updateContact(ContactModel contact, AuthEntity user);
+}
+
+class ContactDatasourceImpl implements ContactDatasource {
+  @override
+  Future<List<ContactModel>?> getContacts(AuthEntity user) async {
+    final preferences = await SharedPreferences.getInstance();
+    final data = preferences.getString('${user.email}-contacts');
+
+    if (data != null) {
+      final contacts = json.decode(data) as List;
+      return contacts.map((e) => ContactModel.fromMap(e)).toList();
+    } else {
+      throw ContactFailure(message: 'Not found');
+    }
+  }
+
+  @override
+  Future<void> addContact(ContactModel contact, AuthEntity user) async {
+    final preferences = await SharedPreferences.getInstance();
+    final data = preferences.getString('${user.email}-contacts');
+
+    if (data != null) {
+      final contacts = json.decode(data) as List;
+
+      contacts.add(contact.toMap());
+      await preferences.setString(
+        '${user.email}-contact',
+        json.encode(contacts),
+      );
+    } else {
+      await preferences.setString(
+        '${user.email}-contact',
+        json.encode([contact.toMap()]),
+      );
+    }
+  }
+
+  @override
+  Future<void> deleteContact(ContactModel contact, AuthEntity user) async {
+    final preferences = await SharedPreferences.getInstance();
+    final data = preferences.getString('${user.email}-contacts');
+
+    if (data != null) {
+      final contacts = json.decode(data) as List;
+
+      contacts.removeWhere((element) => element['cpf'] == contact.cpf);
+      await preferences.setString(
+        '${user.email}-contact',
+        json.encode(contacts),
+      );
+    }
+  }
+
+  @override
+  Future<void> updateContact(ContactModel contact, AuthEntity user) async {
+    final preferences = await SharedPreferences.getInstance();
+    final data = preferences.getString('${user.email}-contacts');
+
+    if (data != null) {
+      final contacts = json.decode(data) as List;
+
+      final index = contacts.indexWhere(
+        (element) => element['cpf'] == contact.cpf,
+      );
+      contacts[index] = contact.toMap();
+      await preferences.setString(
+        '${user.email}-contact',
+        json.encode(contacts),
+      );
+    }
+  }
+}
