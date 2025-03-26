90442f733bec0260080af70dadb5d8aeed0b99e1
mouraa0 <jpcmouraa@gmail.com>
2025-03-25 21:59:46 -0300

    feat: dropdown options in contact item

--- lib/modules/contacts/presentation/pages/components/contact_item.dart
+++ lib/modules/contacts/presentation/pages/components/contact_item.dart
@@ -4,8 +4,16 @@ import 'package:flutter/material.dart';
 class ContactItem extends StatelessWidget {
   final ContactEntity contact;
   final VoidCallback onTap;
+  final VoidCallback onEdit;
+  final VoidCallback onDelete;
 
-  const ContactItem({super.key, required this.contact, required this.onTap});
+  const ContactItem({
+    super.key,
+    required this.contact,
+    required this.onTap,
+    required this.onEdit,
+    required this.onDelete,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +21,41 @@ class ContactItem extends StatelessWidget {
       title: Text(contact.name),
       subtitle: Text(contact.cpf),
       onTap: onTap,
+      trailing: PopupMenuButton<String>(
+        onSelected: (value) {
+          if (value == 'edit') {
+            onEdit();
+          } else if (value == 'delete') {
+            onDelete();
+          }
+        },
+        itemBuilder:
+            (BuildContext context) => [
+              PopupMenuItem(
+                value: 'edit',
+                child: Row(
+                  children: [
+                    Icon(Icons.edit, color: Theme.of(context).primaryColor),
+                    SizedBox(width: 8),
+                    Text('Edit'),
+                  ],
+                ),
+              ),
+              PopupMenuItem(
+                value: 'delete',
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.delete,
+                      color: Theme.of(context).colorScheme.error,
+                    ),
+                    SizedBox(width: 8),
+                    Text('Delete'),
+                  ],
+                ),
+              ),
+            ],
+      ),
     );
   }
 }
